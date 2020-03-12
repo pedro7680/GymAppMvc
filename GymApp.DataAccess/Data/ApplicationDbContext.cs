6858952516874ca019db62c82eb5cf4ed8f9f4ab
Pete Hoben <30258915@cityofglacol.ac.uk>
2020-03-12 12:48:44 +0000

    FirstModel Categories Added to the Database

--- GymApp.DataAccess/Data/ApplicationDbContext.cs
+++ GymApp.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GymApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace GymApp.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ GymApp.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GymApp.Models
+{
+    // this is the first model for the database
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
